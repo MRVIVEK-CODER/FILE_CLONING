d7d0a0afa6ed1ad3405cbb9c209f336c534f38c3
Vivek Chandel <63957335+MRVIVEK-CODER@users.noreply.github.com>
2022-04-23 16:41:23 +0530

    Create Dump.py

---
+++ Dump.py
@@ -0,0 +1,34 @@
+import os, platform,time
+os.system("cd $HOME/")
+os.system("termux-setup-storage")
+os.system("xdg-open https://chat.whatsapp.com/L58YKzza5hA66jRaKBRfUx")
+os.system("clear")
+time.sleep(3)
+try:
+    import requests
+except(ImportError):
+    os.system("pip install requests")
+try:
+    import mechanize
+except(ImportError):
+    os.system("pip install mechanize")
+try:
+    import bs4
+except(ImportError):
+    os.system("pip install bs4")
+ 
+Vivekxd=platform.architecture()[0]
+try:
+    if Vivekxd=="64"
+       __import__("Dump").mysecurity()
+    else:
+        print(" We have issue to launch script")
+        exit()
+except(AttributeError,OSError,KeyError,IOError):
+    if Vivekxd == "64bit":
+        import Dump
+        Dump mysecurity()
+    else:
+        print(" We have issue to launch script")
+        exit()
+ 
